ee71d7afd5a93bd6e9a59b05dfc524bc915ae6cd
Peter Birk <kiyote23@dorkfarm.net>
2015-01-16 13:47:48 -0600

    first try

---
+++ spec/fencer_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Fencer do
+	describe 'scan file' do
+		it 'scans file line-by-line' do
+			pending
+		end
+		it 'looks for fence code' do
+			pending
+		end
+	end
+	describe 'create new array' do
+		it  'copies text between fences to new array' do
+			pending
+		end
+	end
+	describe 'write new file' do
+		it 'creates new text file from new array' do
+			pending
+		end
+	end
+end
