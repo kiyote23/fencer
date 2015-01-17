210a69f718a0c38b1d5f9d9677194ccb0d4fabad
Peter Birk <kiyote23@dorkfarm.net>
2015-01-16 21:04:50 -0600

    got basic spec working

--- lib/fencer.rb
+++ lib/fencer.rb
@@ -1,12 +1,20 @@
 require "fencer/version"
 
 class Fencer
-	attr_accessor :input_array, :fence_start
+	attr_accessor :input_array, :fence_start, :fence_end, :output_array
 	def initialize(input_array)
 		@input_array = input_array
-		@fence_start = find_fence(input_array)
+		find_fence(input_array)
 	end
 
 	def find_fence(input_array)
+		input_array.each.with_index do |string, index|
+			if string =~ /<<</i 
+				self.fence_start = index
+			elsif string =~ />>>/i
+				self.fence_end = index
+			end
+		end
+		self.output_array = input_array[self.fence_start+1...self.fence_end].to_a
 	end
 end
--- spec/fencer_spec.rb
+++ spec/fencer_spec.rb
@@ -3,36 +3,20 @@ require_relative '../lib/fencer'
 
 describe Fencer do
 	describe 'object' do
+		test_array = ["This is a new string.", "This is another string.", "<<< fence start", "This is a fourth string.", ">>> fence end", "This is a final string."]
+		test_object = Fencer.new(test_array)
 		it 'has an array of strings' do
-			fencer_test_array = %w{"this is an array of strings",
-				"We'll add another line here",
-				"One more string"}
-			fencer_test_object = Fencer.new(fencer_test_array)
-			# fencer_test_object.input_array should equal fencer_test_array
-			# fencer_test_object.input_array.should equal(fencer_test_array)
-			expect(fencer_test_object.input_array).to equal(fencer_test_array)
+			expect(test_object.input_array).to equal(test_array)
 		end
 		it 'looks for array elements starting with fence delimiter' do
-			test_array = %w{"This is the first string.",
-				"This is the second string.",
-				"<<< This is the fence",
-				"This is the last string."}
-			test_object = Fencer.new(test_array)
-			# test_object.fence_start.should equal(2)	
 			expect(test_object.fence_start).to eq(2)
 		end
-		it 'looks for fence code' do
-			pending
+		it 'looks for the ending fence delimiter' do
+			expect(test_object.fence_end).to eq(4)
 		end
-	end
-	describe 'create new array' do
 		it  'copies text between fences to new array' do
-			pending
-		end
-	end
-	describe 'write new file' do
-		it 'creates new text file from new array' do
-			pending
+			output_array = ["This is a fourth string."]
+			expect(test_object.output_array).to eq(output_array)
 		end
 	end
 end
